4b667fbe9d3727473b6c621376d5c8f97206f824
username <StrugglingYJ-0512.name>
2021-07-17 19:34:27 +0900

    express - 라우터의 주소 체계 ( 메인페이지 ('/')를 제외한 페이지의 경로를 /topic/으로 시작하도록 변경함.

--- main.js
+++ main.js
@@ -31,16 +31,109 @@ app.get('/', (request, response) => {
   var html = template.HTML(title, list,
     `<h2>${title}</h2>${description},
     <img src="/images/hello.jpg" style="width:400px; display:block; margin-top:10px">`,
-    `<a href="/create">create</a>`
+    `<a href="/topic/create">create</a>`
   );
   response.send(html);
 
 })
 
-// 2. 상세페이지 구현
+// 3.create 
+app.get('/topic/create', (request, response) => {
+  var title = 'WEB - create';
+  var list = template.list(request.list);
+  var html = template.HTML(title, list, `
+        <form action="/topic/create" method="post">
+          <p><input type="text" name="title" placeholder="title"></p>
+          <p>
+            <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+      `, '');
+  response.send(html);
+
+})
+app.post('/topic/create', (request, response) => {
+  console.log(request.list)
+  //post는 body-parser 부분!
+  var post = request.body;
+  var title = post.title;
+  var description = post.description;
+  fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+    // response.writeHead(302, { Location: `/topic/${title}` });
+    // response.end();
+    response.redirect(`/topic/${title}`); //리다이렉트 
+  });
+})
+
+//4.Update
+app.get('/topic/update/:pageId', (request, response) => {
+  var filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
+    var title = request.params.pageId;
+    var list = template.list(request.list);
+    var html = template.HTML(title, list,
+      `
+          <form action="/topic/update_process" method="post">
+            <input type="hidden" name="id" value="${title}">
+            <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+            <p>
+              <textarea name="description" placeholder="description">${description}</textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+          `,
+      `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
+    );
+    response.send(html);
+  });
+
+})
+
+app.post('/topic/update_process', function (request, response) {
+  //post는 body-parser 부분!
+  var post = request.body;
+  var id = post.id;
+  var title = post.title;
+  var description = post.description;
+  fs.rename(`data/${id}`, `data/${title}`, function (error) {
+    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+      // response.writeHead(302, { Location: `/topic/${title}` });
+      // response.end();
+      response.redirect(`/topic/${title}`);
+    })
+  });
+});
+
+// 5. Delete
+app.post('/topic/delete_process', function (request, response) {
+  //post는 body-parser 부분!
+  var post = request.body;
+  var id = post.id;
+  var filteredId = path.parse(id).base;
+  fs.unlink(`data/${filteredId}`, function (error) {
+    response.redirect('/');
+    // express에서 제공하는 redirect 
+    // Google : nodejs express redirect 로 검색
+    /*기존: 
+      response.writeHead(302, { Location: `/` });
+      response.end();*/
+  })
+});
+
+app.use(function (req, res, next) {
+  res.status(404).send('Sorry cant find that!');
+});
+
+
+// 2. 상세페이지 구현 (/topic/path로 들어온 페이지 처리)
 // 시멘틱  url 로 작성 ; queryString을 쓰지 않고, path로만 작성
 // Err가 있는 경우 next를 호출한다. 
-app.get('/page/:pageId', (request, response, next) => {
+app.get('/topic/:pageId', (request, response, next) => {
   var filteredId = path.parse(request.params.pageId).base;
   fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
     /* fs.readFile(`data/${filteredId}`, 'utf8', function (err, description)
@@ -60,9 +153,9 @@ app.get('/page/:pageId', (request, response, next) => {
       var list = template.list(request.list);
       var html = template.HTML(sanitizedTitle, list,
         `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/create">create</a>
-              <a href="/update/${sanitizedTitle}">update</a>
-              <form action="/delete_process" method="post">
+        ` <a href="/topic/create">create</a>
+              <a href="/topic/update/${sanitizedTitle}">update</a>
+              <form action="/topic/delete_process" method="post">
                 <input type="hidden" name="id" value="${sanitizedTitle}">
                 <input type="submit" value="delete">
               </form>`
@@ -75,103 +168,10 @@ app.get('/page/:pageId', (request, response, next) => {
 
 
 
-// 3.create 
-/* action 의 path : 
- 왜 같은 /create 경로로 전송하지??? 
- 접근 할 때 get 방식으로 접근하면, app.get('/create')에 걸릴것이고
- 데이터를 전홀 할 때, post방식으로 접근하면, app.post('/create')에 걸린다.!!
-*/
-app.get('/create', (request, response) => {
-  var title = 'WEB - create';
-  var list = template.list(request.list);
-  var html = template.HTML(title, list, `
-        <form action="/create" method="post">
-          <p><input type="text" name="title" placeholder="title"></p>
-          <p>
-            <textarea name="description" placeholder="description"></textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-      `, '');
-  response.send(html);
-
-})
-
-app.post('/create', (request, response) => {
-  console.log(request.list)
-  //post는 body-parser 부분!
-  var post = request.body;
-  var title = post.title;
-  var description = post.description;
-  fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-    response.writeHead(302, { Location: `/page/${title}` });
-    response.end();
-  });
-})
-
-//4.Update
-app.get('/update/:pageId', (request, response) => {
-  var filteredId = path.parse(request.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-    var title = request.params.pageId;
-    var list = template.list(request.list);
-    var html = template.HTML(title, list,
-      `
-          <form action="/update_process" method="post">
-            <input type="hidden" name="id" value="${title}">
-            <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-            <p>
-              <textarea name="description" placeholder="description">${description}</textarea>
-            </p>
-            <p>
-              <input type="submit">
-            </p>
-          </form>
-          `,
-      `<a href="/create">create</a> <a href="/update/${title}">update</a>`
-    );
-    response.send(html);
-  });
-
-})
-
-app.post('/update_process', function (request, response) {
-  //post는 body-parser 부분!
-  var post = request.body;
-  var id = post.id;
-  var title = post.title;
-  var description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, function (error) {
-    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-      // response.writeHead(302, { Location: `/page/${title}` });
-      // response.end();
-      response.redirect(`/page/${title}`);
-    })
-  });
-});
 
 
-// 5. Delete
-app.post('/delete_process', function (request, response) {
-  //post는 body-parser 부분!
-  var post = request.body;
-  var id = post.id;
-  var filteredId = path.parse(id).base;
-  fs.unlink(`data/${filteredId}`, function (error) {
-    response.redirect('/');
-    // express에서 제공하는 redirect 
-    // Google : nodejs express redirect 로 검색
-    /*기존: 
-      response.writeHead(302, { Location: `/` });
-      response.end();*/
-  })
-});
 
-app.use(function (req, res, next) {
-  res.status(404).send('Sorry cant find that!');
-});
+
 
 /* status Code : 404 밑에 놔야함.
 *  이, Err Handling을 위한 middleWare는 특이하게도,4개의 인자를 받는다. 
