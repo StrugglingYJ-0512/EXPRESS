7844c1fc859c453609c578d99a865952b54df074
username <StrugglingYJ-0512.name>
2021-07-17 18:49:57 +0900

    express - Err
    1. 상세페이지를 출력시, 목록에 없는 파일을 url으로 접근시 Err 메시지 나오도록 작업.
    2. middleWare에서 param이 4개인 함수는 무조건 Err 처리 함수로 약속함.
    3. 위치는 맨 끝에 놓는다. 위에서 차례로 미들웨어를 실행하고, 해당하는 값이 없을 경우 내려오도록.

--- main.js
+++ main.js
@@ -39,27 +39,38 @@ app.get('/', (request, response) => {
 
 // 2. 상세페이지 구현
 // 시멘틱  url 로 작성 ; queryString을 쓰지 않고, path로만 작성
-app.get('/page/:pageId', (request, response) => {
+// Err가 있는 경우 next를 호출한다. 
+app.get('/page/:pageId', (request, response, next) => {
   var filteredId = path.parse(request.params.pageId).base;
   fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-    var title = request.params.pageId;
-    var sanitizedTitle = sanitizeHtml(title);
-    var sanitizedDescription = sanitizeHtml(description, {
-      allowedTags: ['h1']
-    });
-    var list = template.list(request.list);
-    var html = template.HTML(sanitizedTitle, list,
-      `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-      ` <a href="/create">create</a>
-            <a href="/update/${sanitizedTitle}">update</a>
-            <form action="/delete_process" method="post">
-              <input type="hidden" name="id" value="${sanitizedTitle}">
-              <input type="submit" value="delete">
-            </form>`
-    );
-    response.send(html);
-  });
+    /* fs.readFile(`data/${filteredId}`, 'utf8', function (err, description)
+     ; data/${filteredId}에서 file을 읽는데, 그 파일이 없다면 callback함수의 
+     첫번째 param으로 Error 객체를 전달하도록 되어잇따. */
+    if (err) { //err 가 발생한다면
+      next(err); // next를 통해서 다음 middleWare를 던져준다.
+      // 인자로 'route'를 전달해도 다음 미들웨어를 실행시킴
+      // 그 외의 데이터가 들어오면, err라는 약속 체결되어있다.
+      // next(err); 는 err데이터를 전달해 주면, err를 던지는 것이다
+    } else {
+      var title = request.params.pageId;
+      var sanitizedTitle = sanitizeHtml(title);
+      var sanitizedDescription = sanitizeHtml(description, {
+        allowedTags: ['h1']
+      });
+      var list = template.list(request.list);
+      var html = template.HTML(sanitizedTitle, list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/create">create</a>
+              <a href="/update/${sanitizedTitle}">update</a>
+              <form action="/delete_process" method="post">
+                <input type="hidden" name="id" value="${sanitizedTitle}">
+                <input type="submit" value="delete">
+              </form>`
+      );
+      response.send(html);
+    }
 
+  });
 })
 
 
@@ -158,7 +169,24 @@ app.post('/delete_process', function (request, response) {
   })
 });
 
+app.use(function (req, res, next) {
+  res.status(404).send('Sorry cant find that!');
+});
 
+/* status Code : 404 밑에 놔야함.
+*  이, Err Handling을 위한 middleWare는 특이하게도,4개의 인자를 받는다. 
+  위에서 실행 된 **_next(err)함수 로부터_** 첫번째 Err인자에 Err 데이터가 담긴다.
+* _4개의 인자를 가지고 있는 함수_는
+  express에서는 Err를 핸들링 하기 위한 MiddleWare로 약속이 되어 있다.. 
+* 실행 순서 > 
+  1. 상세페이지에서 페이지를 찾을 수 없는 경우 _**if(err)**_에 걸려, next(err) 함수가 실행
+    2. 이 next(err) 함수 : 뒤로 어떤 미들웨어가 등록되어 있든지 싹 다 무시하고 제일_** 맨 끝의 밑의 인자가 4개인 함수로**_서 등록된 미들웨어가 호출된다.
+    3. 그 첫번째 자리로 Err 데이터가 전달된다. 
+*/
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
